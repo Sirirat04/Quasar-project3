7740c88605bf603797588638df5632ed80705ebe
Sirirat04 <wanpen.kpp1234@gmail.com>
2024-09-11 14:45:59 +0700

    initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form>
+      <input type="text" id="firstName" v-model="firstName" />
+      <input type="text" id="secondName" v-model="secondName" />
+      <button @click="submitForm()" class="submitForm">Submit</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputPage',
+  data() {
+    return {
+      title: 'วิวัฒนาการของโลกสมัยใหม่.',
+      nickname: 'หมิว',
+      firstName: 'ศิริรัตน์',
+      secondName: 'คชสาร',
+      studentCode : '6604101382',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form')
+    },
+  }
+}
+</script>
+
+<style scoped></style>
